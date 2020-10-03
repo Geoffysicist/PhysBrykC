0ae6fe06166cac8ef5a3bed2bb0f1a7d147d32e0
Geoffysicist <66104692+Geoffysicist@users.noreply.github.com>
2020-10-04 00:09:38 +1000

    reverted to older library
    latest version of APDS9960 breaks things

--- examples/proximity/proximity.ino
+++ examples/proximity/proximity.ino
@@ -2,6 +2,7 @@
 #include <Arduino_APDS9960.h>
 
 PhysBryk thisBryk(LED_BUILTIN);
+bool brykConnected;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -9,12 +10,14 @@ void setup() {
     Serial.begin(115200);
     // while (!Serial) delay(10);
     Serial.println("PhysBryk Proximity Demo");
-    if (thisBryk.begin()) Serial.println("the Bryk is ready");
+    if (thisBryk.begin()) brykConnected = true;
     // else Serial.println("the Bryk is stuffed")
   
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  thisBryk.blynk();
+  Serial.println(brykConnected);
   
 }
